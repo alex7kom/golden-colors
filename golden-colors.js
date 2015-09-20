65ea7f143e0a28d63b53357fb0234ba3942cd467
Alexey Komarov <alex7kom@gmail.com>
2015-09-20 14:19:43 +0300

    Improved code style

--- golden-colors.js
+++ golden-colors.js
@@ -8,20 +8,21 @@
 */
 
 (function() {
-  
-  function getRandom255() {
+  var goldenRatioConjugate = 0.618033988749895;
+
+  function getRandom255 () {
     return Math.round(Math.random() * 255);
   }
 
-  function convertDecToHex(number) {
+  function convertDecToHex (number) {
     var result = number.toString(16);
-    if (result.length == 1) {
+    if (result.length === 1) {
       result = '0' + result;
     }
     return result;
   }
 
-  function convertHsvToRgb(h, s, v) {
+  function convertHsvToRgb (h, s, v) {
     var r, g, b;
 
     var i = Math.floor(h * 6);
@@ -42,59 +43,57 @@
     return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
   }
 
-  function WebColor(r, g, b) {
+  function WebColor (r, g, b) {
     this.r = r;
     this.g = g;
     this.b = b;
   }
 
-  WebColor.prototype.toRgb = function() {
+  WebColor.prototype.toRgb = function () {
     return [this.r, this.g, this.b];
   };
 
-  WebColor.prototype.toRgbString = function() {
+  WebColor.prototype.toRgbString = function () {
     return 'rgb(' + this.r + ', ' + this.g + ', ' + this.b + ')';
   };
 
-  WebColor.prototype.toHexString = function() {
+  WebColor.prototype.toHexString = function () {
     return '#' +
       convertDecToHex(this.r) +
       convertDecToHex(this.g) +
       convertDecToHex(this.b);
   };
 
-  WebColor.prototype.toString = function() {
+  WebColor.prototype.toString = function () {
     return this.toHexString();
   };
 
-  WebColor.prototype.toJSON = function() {
+  WebColor.prototype.toJSON = function () {
     return this.toHexString();
   };
 
-  var GoldenColors = function() {
-    this.goldenRatioConjugate = 0.618033988749895;
-  };
+  var goldenColors = {};
 
-  GoldenColors.prototype.getNaive = function() {
+  goldenColors.getNaive = function () {
     return new WebColor(getRandom255(), getRandom255(), getRandom255());
   };
 
-  GoldenColors.prototype.getHsvSimple = function(s, v) {
+  goldenColors.getHsvSimple = function (s, v) {
     var color = convertHsvToRgb(Math.random(), s, v);
     return new WebColor(color[0], color[1], color[2]);
   };
 
-  GoldenColors.prototype.getHsvGolden = function(s, v) {
+  goldenColors.getHsvGolden = function (s, v) {
     var h = Math.random();
-    h += this.goldenRatioConjugate;
+    h += goldenRatioConjugate;
     h %= 1;
     var color = convertHsvToRgb(h, s, v);
     return new WebColor(color[0], color[1], color[2]);
   };
 
   if (typeof module !== 'undefined') {
-    module.exports = new GoldenColors();
+    module.exports = goldenColors;
   } else if (typeof window !== 'undefined') {
-    window.goldenColors = new GoldenColors();
+    window.goldenColors = goldenColors;
   }
 })();
